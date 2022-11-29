bf0d66b19e73a8749f757a15874ad4a51a3b48ae
123ilya <123ilya@gmail.com>
2022-11-29 23:35:23 +1000

    Добавил автозагрузчик composer

--- autoload.php
+++ autoload.php
@@ -4,5 +4,6 @@ function loaderEntities($className)
 {
     require_once 'entities' . '/' . $className . '.php';
 }
-
+//Подключаем автозагрузчик Composer к проекту
+require_once 'vendor/autoload.php';
     spl_autoload_register('loaderEntities');
