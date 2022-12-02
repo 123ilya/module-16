6c7309dc1d186db878a9558cea05833c45007d0c
123ilya <123ilya@gmail.com>
2022-12-02 12:28:28 +1000

    Добавил форму

---
+++ input_text.php
@@ -0,0 +1,30 @@
+<?php
+    require_once 'autoload.php';
+?>
+    
+    <!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Форма для отправки текстов</title>
+</head>
+<body>
+<h1>Форма для отправки текстов в "ТЕЛЕГРАФ" </h1>
+<form method="post" action="input_text.php">
+    <label>Автор:
+        <input type="text" name="author" >
+    </label>
+    <label>
+        Текст:
+        <input type="text" name="text" >
+    </label>
+    <label>
+        Email:
+        <input type="email" name="email">
+    </label>
+    <label>
+        <input type="submit" value="Отправить">
+    </label>
+</form>
+</body>
+</html>
