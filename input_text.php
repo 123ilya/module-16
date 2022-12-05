7f0da72ed4bf79364d08524111cd06a2d7a0fb68
123ilya <123ilya@gmail.com>
2022-12-05 22:46:28 +1000

    test messege is sent

--- input_text.php
+++ input_text.php
@@ -1,4 +1,7 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
     
     require_once 'autoload.php';
 ?>
@@ -35,8 +38,29 @@
         $fileStorage=new FileStorage();
         $fileStorage->create($telegraphText);
     }
+//    Рассылка почты
+//    Создаём экземпляр класса PHPMailer
+// Создаем письмо
+    $mail = new PHPMailer();
+    $mail->isSMTP();                   // Отправка через SMTP
+    $mail->Host   = 'smtp.yandex.ru';  // Адрес SMTP сервера
+    $mail->SMTPAuth   = true;          // Enable SMTP authentication
+    $mail->Username   = 'ilyasobolev8400';       // ваше имя пользователя (без домена и @)
+    $mail->Password   = 'dvqyslasbjrcqame';    // ваш пароль
+    $mail->SMTPSecure = 'ssl';         // шифрование ssl
+    $mail->Port   = 465;               // порт подключения
     
+    $mail->setFrom('ilyasobolev8400@yandex.ru', 'ilya');    // от кого
+    $mail->addAddress('ilyasobolev8400@yandex.ru', 'Вася Петров'); // кому
     
-   
-    
-       
+    $mail->Subject = 'Тест';
+    $mail->msgHTML("<html><body>
+                <h1>Здравствуйте!</h1>
+                <p>Это тестовое письмо.</p>
+                </html></body>");
+// Отправляем
+    if ($mail->send()) {
+        echo 'Письмо отправлено!';
+    } else {
+        echo 'Ошибка: ' . $mail->ErrorInfo;
+    }
